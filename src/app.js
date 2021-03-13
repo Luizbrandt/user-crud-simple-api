f9902369c56b78dcdf1d280f8a5acda87613358f
André Luiz Brandt <andre-luiz.b@unifei.edu.br>
2021-03-13 16:59:04 -0300

    Insert cors

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ app.use("/api/users", routeUsers);
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const connectionObj = {
     useNewUrlParser: true,
